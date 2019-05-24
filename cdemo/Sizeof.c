52450f6e95f48909fd58caa1b29d06de170356a4
leej6257 <50209412+leej6257@users.noreply.github.com>
2019-05-24 09:01:42 -0700

    Create Sizeof.c

---
+++ cdemo/Sizeof.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  
+  char b = 'a';
+  printf("char b value: %c and size %d bytes\n", b, sizeof(b));
+
+  float c = 12.345;
+  printf("float c value: %f and size %d bytes\n", c, sizeof(c));
+
+  double d = 123.456;
+  printf("double d value: %f and size %d bytes\n", d, sizeof(d));
+
+  unsigned int e = -99;
+  printf("int e value: %d and size %d bytes\n", e, sizeof(e));
+
+  signed int f = -123;
+  printf("int f value: %d and size %d bytes\n", f, sizeof(f));
+}
